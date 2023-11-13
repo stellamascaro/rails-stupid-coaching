d5d248689133069e5f90a9db2b5fb78ce9aafef3
stella mascaro <stellamascaro@hotmail.com>
2023-11-13 17:13:33 -0300

    .

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question.end_with?('?') == true
+      @coach_answer = 'Silly question, get dressed and go to work!'
+
+    elsif @question == 'I am going to work'
+      @coach_answer = 'Great!'
+
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
